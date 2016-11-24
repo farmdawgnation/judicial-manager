5e359b12011323bb4c3304d669da4957d0d18924
Matt Farmer <matt@frmr.me>
2016-11-24 11:53:22 -0500

    Development of the match BDOs.

--- src/main/scala/frmr/scyig/models/RoundMatch.scala
+++ src/main/scala/frmr/scyig/models/RoundMatch.scala
@@ -15,13 +15,100 @@
 **/
 package frmr.scyig.models
 
+/**
+ * Representation of a kind of match in a round at the Judicial conference. Matches may be partial
+ * or complete. Partial matches are largely used during the matching algorithm to progressively
+ * develop the matches.
+ */
 sealed trait RoundMatch
-case class TrialMatch(
+
+/**
+ * Parent trait for any kind of match that is incomplete and therefore not capable of being
+ * scheduled.
+ */
+sealed trait PartialRoundMatch extends RoundMatch {
+  def prosecution: CompetingTeam
+  def defense: CompetingTeam
+
+  def toByes: Seq[Bye] = {
+    Seq(Bye(prosecution), Bye(defense))
+  }
+}
+
+/**
+ * Parent trait for any completely scheduled match. This could be an actual [[Trial]] or it could be
+ * a [[Bye]].
+ */
+sealed trait CompletedRoundMatch extends RoundMatch
+
+/**
+ * A partial match in which the teams have been matched.
+ */
+case class MatchedTeams(
+  prosecution: CompetingTeam,
+  defense: CompetingTeam
+) extends PartialRoundMatch {
+  def withPresidingJudge(presidingJudge: PresidingJudge): MatchedTeamsWithPresidingJudge = {
+    MatchedTeamsWithPresidingJudge(
+      prosecution,
+      defense,
+      presidingJudge
+    )
+  }
+}
+
+/**
+ * A partial match in which the teams have a presiding judge.
+ */
+case class MatchedTeamsWithPresidingJudge(
+  override val prosecution: CompetingTeam,
+  override val defense: CompetingTeam,
+  presidingJudge: PresidingJudge
+) extends PartialRoundMatch {
+  def withScoringJudge(scoringJudge: ScoringJudge): MatchedTeamsWithPresidingAndScoringJudge = {
+    MatchedTeamsWithPresidingAndScoringJudge(
+      prosecution,
+      defense,
+      presidingJudge,
+      scoringJudge
+    )
+  }
+}
+
+/**
+ * A parital match in which the teams have a presiding judge and a scoring judge.
+ */
+case class MatchedTeamsWithPresidingAndScoringJudge(
+  override val prosecution: CompetingTeam,
+  override val defense: CompetingTeam,
+  presidingJudge: PresidingJudge,
+  scoringJudge: ScoringJudge
+) extends PartialRoundMatch {
+  def withRoom(roomNumber: Int): Trial = {
+    Trial(
+      prosecution,
+      defense,
+      presidingJudge,
+      scoringJudge,
+      roomNumber
+    )
+  }
+}
+
+/**
+ * A completely scheduled trial match.
+ */
+case class Trial(
   prosecution: CompetingTeam,
   defense: CompetingTeam,
   presidingJudge: PresidingJudge,
-  scoringJudge: ScoringJudge
-)
-case class ByeMatch(
+  scoringJudge: ScoringJudge,
+  roomNumber: Int
+) extends CompletedRoundMatch
+
+/**
+ * A Bye match, representing where a team is not playing another team.
+ */
+case class Bye(
   team: CompetingTeam
-)
+) extends CompletedRoundMatch
