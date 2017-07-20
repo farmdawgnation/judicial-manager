05cb9dba040aec488b6a044d64348b5c43811ee2
Matt Farmer <matt@frmr.me>
2017-07-19 21:30:47 -0400

    Add Competitions

---
+++ src/main/scala/frmr/scyig/db/Competitions.scala
@@ -0,0 +1,25 @@
+package frmr.scyig.db
+
+import slick.jdbc.H2Profile.api._
+
+case class Competition(
+  id: Option[Int],
+  name: String,
+  sponsorId: Int,
+  dates: String,
+  description: String
+)
+
+class Competitions(tag: Tag) extends Table[Competition](tag, "competitions") {
+  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("name")
+  def sponsorId = column[Int]("sponsor_id")
+  def dates = column[String]("dates")
+  def description = column[String]("description")
+
+  def * = (id.?, name, sponsorId, dates, description) <> (Competition.tupled, Competition.unapply)
+
+  def sidFK = foreignKey("sponsor_id_fk", sponsorId, Sponsors)(_.id, onDelete = ForeignKeyAction.Cascade)
+}
+
+object Competitions extends TableQuery[Competitions](new Competitions(_))
