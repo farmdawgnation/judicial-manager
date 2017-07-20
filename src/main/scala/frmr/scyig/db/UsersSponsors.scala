92ea8d07feee8a804c824cc2f3e802b501dfd7a9
Matt Farmer <matt@frmr.me>
2017-07-19 21:22:16 -0400

    Add UsersSponsors definition

---
+++ src/main/scala/frmr/scyig/db/UsersSponsors.scala
@@ -0,0 +1,14 @@
+package frmr.scyig.db
+
+import slick.jdbc.H2Profile.api._
+
+class UsersSponsors(tag: Tag) extends Table[(String, Int, Int)](tag, "users_sponsors") {
+  def role = column[String]("role")
+  def userId = column[Int]("user_id")
+  def sponsorId = column[Int]("sponsor_id")
+
+  def * = (role, userId, sponsorId)
+
+  def uidFK = foreignKey("user_id_fk", userId, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
+  def sidFK = foreignKey("sponsor_id_fk", sponsorId, Sponsors)(_.id, onDelete = ForeignKeyAction.Cascade)
+}
