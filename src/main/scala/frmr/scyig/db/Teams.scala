511ed8d8327debbb9281813c7745137ed1922b31
Matt Farmer <matt@frmr.me>
2017-07-19 21:35:13 -0400

    Add Teams

---
+++ src/main/scala/frmr/scyig/db/Teams.scala
@@ -0,0 +1,23 @@
+package frmr.scyig.db
+
+import slick.jdbc.H2Profile.api._
+
+case class Team(
+  id: Option[Int],
+  competitionId: Int,
+  name: String,
+  organization: String
+)
+
+class Teams(tag: Tag) extends Table[Team](tag, "teams") {
+  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  def competitionId = column[Int]("competition_id")
+  def name = column[String]("name")
+  def organization = column[String]("organization")
+
+  def * = (id.?, competitionId, name, organization) <> (Team.tupled, Team.unapply)
+
+  def cidFK = foreignKey("competition_id_fk", competitionId, Competitions)(_.id, onDelete = ForeignKeyAction.Cascade)
+}
+
+object Teams extends TableQuery[Teams](new Teams(_))
