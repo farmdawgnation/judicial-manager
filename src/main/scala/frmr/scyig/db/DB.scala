d29f35fb1a322658cf567b24a4eb16d418f60dd0
Matt Farmer <matt@frmr.me>
2017-07-20 22:14:25 -0400

    Intelligently avoid DB creation

--- src/main/scala/frmr/scyig/db/DB.scala
+++ src/main/scala/frmr/scyig/db/DB.scala
@@ -2,10 +2,12 @@ package frmr.scyig.db
 
 import scala.concurrent._
 import scala.concurrent.duration._
+import slick.jdbc.meta._
 import slick.jdbc.MySQLProfile.api._
+import net.liftweb.common._
 import net.liftweb.util._
 
-object DB {
+object DB extends Loggable {
   val database = {
     for {
       url <- Props.get("database.url") ?~! "No database url"
@@ -15,18 +17,26 @@ object DB {
       Database.forURL(
         url,
         username,
-        password
+        password,
+        driver = "com.mysql.cj.jdbc.Driver"
       )
     }
   } openOrThrowException("Database information is required.")
 
   def createSchema(): Unit = {
-    val setup = DBIO.seq(
-      (Competitions.schema ++ Judges.schema ++ Matches.schema ++ Scores.schema ++ Sponsors.schema ++
-        Teams.schema ++ Users.schema ++ UsersSponsors.schema).create
-    )
+    val tables: Vector[MTable] = Await.result(database.run(MTable.getTables("%")), Duration(30, SECONDS))
 
-    val schemaFuture = database.run(setup)
-    Await.result(schemaFuture, Duration(30, SECONDS))
+    if (tables.length == 0) {
+      logger.info("Tables do not already exist. Creating schemas.")
+      val setup = DBIO.seq(
+        (Competitions.schema ++ Judges.schema ++ Matches.schema ++ Scores.schema ++ Sponsors.schema ++
+          Teams.schema ++ Users.schema ++ UsersSponsors.schema).create
+      )
+
+      val schemaFuture = database.run(setup)
+      Await.result(schemaFuture, Duration(30, SECONDS))
+    } else {
+      logger.info("Tables already exist. Not creating schemas.")
+    }
   }
 }
