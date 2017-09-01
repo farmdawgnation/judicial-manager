133cf225460285bbd82a0c1b53ca8ac334483796
Matt Farmer <matt@frmr.me>
2017-09-01 16:38:17 -0400

    Implement competition list page

--- src/main/scala/frmr/scyig/webapp/snippet/CompChooser.scala
+++ src/main/scala/frmr/scyig/webapp/snippet/CompChooser.scala
@@ -6,6 +6,8 @@ import net.liftweb.common._
 import net.liftweb.http._
 import net.liftweb.sitemap._
 import net.liftweb.sitemap.Loc._
+import net.liftweb.util._
+import net.liftweb.util.Helpers._
 import slick.jdbc.MySQLProfile.api._
 
 object CompChooser {
@@ -13,13 +15,23 @@ object CompChooser {
     If(() => S.loggedIn_?, RedirectResponse(Login.menu.loc.calcDefaultHref))
 
 
-  val competitionsQuery = Users.to[List].filter(_.id === currentUserId.is)
+  def competitionsQuery = Users.to[List].filter(_.id === currentUserId.is)
     .join(UsersSponsors).on(_.id === _.userId)
     .map(_._2)
     .join(Competitions).on(_.sponsorId === _.sponsorId)
     .map(_._2)
     .result
 
-  def listCompetitions: Box[List[Competition]] =
+  private[this] def listCompetitions: Box[List[Competition]] =
     DB.runAwait(competitionsQuery)
+
+  def render = {
+    ClearClearable andThen
+    ".competition-entry" #> listCompetitions.openOr(Nil).map { competition =>
+      ".competition-selection-title *" #> competition.name &
+      ".competition-selection-location *" #> competition.description &
+      ".competition-selection-date *" #> competition.dates &
+      ".select-competition [href]" #> CompDashboard.menu.toLoc.calcHref(competition)
+    }
+  }
 }
