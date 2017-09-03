86e2d4aac82f9ff9cafd981208d4203410555cb9
Matt Farmer <matt@frmr.me>
2017-09-03 12:22:50 -0400

    Enable navigating back to the competiton chooser
    Eventually we'll make this link smart and have it take you back to a single dashboard if you're only
    on one competition, but it's more important for now that we make navigation throughout the system
    easier as currently its a bit impossible

--- src/main/scala/frmr/scyig/webapp/snippet/CurrentUser.scala
+++ src/main/scala/frmr/scyig/webapp/snippet/CurrentUser.scala
@@ -4,6 +4,8 @@ import frmr.scyig.webapp.auth.AuthenticationHelpers._
 import net.liftweb.util.Helpers._
 
 object CurrentUser {
+  def rootLink =
+    "^ [href]" #> CompChooser.menu.loc.calcDefaultHref
   def userName =
     "^ *" #> currentUser.is.map(_.name).getOrElse("A Ghost")
 
