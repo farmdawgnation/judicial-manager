5685712a56e10eb250aab9f34949a973accac123
Matt Farmer <matt@frmr.me>
2017-09-03 18:48:41 -0400

    Improve click UX on competition chooser

---
+++ src/main/webapp/js/application.js
@@ -0,0 +1,14 @@
+(function() {
+  $("#select-competition-container").on('click', '.competition-entry', function(event) {
+    var $target = $(event.target);
+
+    if (! $target.is(".select-competition")) {
+      var href = $(event.target)
+        .closest(".competition-entry")
+        .find(".select-competition")
+        .attr('href');
+
+      window.location.href = href;
+    }
+  });
+})();
