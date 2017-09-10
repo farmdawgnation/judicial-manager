3f42893353532fd6457a37f47c8ccceb6b7dc86d
Matt Farmer <matt@frmr.me>
2017-09-09 23:07:56 -0400

    Add team id to scores

---
+++ src/main/resources/db/migration/V1.5__Add_teamid_to_score.sql
@@ -0,0 +1,1 @@
+alter table scores add column `team_id` integer not null;
--- src/main/scala/frmr/scyig/db/Scores.scala
+++ src/main/scala/frmr/scyig/db/Scores.scala
@@ -4,16 +4,18 @@ import slick.jdbc.MySQLProfile.api._
 
 case class Score(
   matchId: Int,
+  teamId: Int,
   scorerId: Int,
   score: Int
 )
 
 class Scores(tag: Tag) extends Table[Score](tag, "scores") {
   def matchId = column[Int]("match_id")
+  def teamId = column[Int]("team_id")
   def scorerId = column[Int]("scorer_id")
   def score = column[Int]("score")
 
-  def * = (matchId, scorerId, score) <> (Score.tupled, Score.unapply)
+  def * = (matchId, teamId, scorerId, score) <> (Score.tupled, Score.unapply)
 }
 
 object Scores extends TableQuery[Scores](new Scores(_))
