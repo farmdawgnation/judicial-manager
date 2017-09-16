8812ebadb8b8439c6bd523a68bb6dce2eba9ecbb
Matt Farmer <matt@frmr.me>
2017-09-16 13:54:48 -0400

    Correct UUID data type

--- src/main/resources/db/migration/V1.6__Add_uuids.sql
+++ src/main/resources/db/migration/V1.6__Add_uuids.sql
@@ -1,8 +1,8 @@
-ALTER TABLE `judges` ADD COLUMN `uuid` VARCHAR(36) NULL;
+ALTER TABLE `judges` ADD COLUMN `uuid` BINARY(16) NULL;
 UPDATE `judges` SET uuid = (SELECT uuid());
 
-ALTER TABLE `teams` ADD COLUMN `uuid` VARCHAR(36) NULL;
+ALTER TABLE `teams` ADD COLUMN `uuid` BINARY(16) NULL;
 UPDATE `teams` SET uuid = (SELECT uuid());
 
-ALTER TABLE `matches` ADD COLUMN `uuid` VARCHAR(36) NULL;
+ALTER TABLE `matches` ADD COLUMN `uuid` BINARY(16) NULL;
 UPDATE `matches` SET uuid = (SELECT uuid());
