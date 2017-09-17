be35a105dabf74abd661384c973fe21458313c7d
Matt Farmer <matt@frmr.me>
2017-09-17 18:53:11 -0400

    Implement a roleOptimizer
    In these judicial competitions, we want to try and ensure that teams have had the same role roughly
    the same nubmer of times. This optimizer does a trivial search over the scheduled trials and swaps
    prosecution/defense roles where doing so would help this objective.

--- src/main/scala/frmr/scyig/matching/MatchingOptmizer.scala
+++ src/main/scala/frmr/scyig/matching/MatchingOptmizer.scala
@@ -4,4 +4,30 @@ import frmr.scyig.matching.models._
 
 object MatchingOptimizer {
   def noOpOptmizer(rounds: Seq[ScheduledRoundMatch]) = rounds
+
+  /**
+   * Attempts to even out the number of times a particular team plays prosecution or defense.
+   */
+  def roleOpimizer(rounds: Seq[ScheduledRoundMatch]) = rounds.map {
+    case byeRound: Bye =>
+      byeRound
+
+    case trialRound: Trial =>
+      val prosecutionRoleScore =
+        trialRound.prosecution.prosecutionCount - trialRound.prosecution.defenseCount
+      val defenseRoleScore =
+        trialRound.defense.prosecutionCount - trialRound.defense.defenseCount
+
+      val swappingRolesWouldHelp =
+        prosecutionRoleScore > 0 && defenseRoleScore < 0
+
+      if (swappingRolesWouldHelp) {
+        trialRound.copy(
+          prosecution = trialRound.defense,
+          defense = trialRound.prosecution
+        )
+      } else {
+        trialRound
+      }
+  }
 }
