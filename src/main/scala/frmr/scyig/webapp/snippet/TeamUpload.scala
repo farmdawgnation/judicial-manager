4782d3889476ec9d13026fd079c5b6cab5e75287
Matt Farmer <matt@frmr.me>
2017-09-16 22:41:54 -0400

    Hack together an okayish bulk upload for teams

--- src/main/scala/frmr/scyig/webapp/snippet/TeamUpload.scala
+++ src/main/scala/frmr/scyig/webapp/snippet/TeamUpload.scala
@@ -22,3 +22,34 @@ object TeamUpload {
   ) / "competition" / * / "teams" / "upload" >>
     validateCompetitionAccess
 }
+
+class TeamUpload(competition: Competition) {
+  val competitionId = competition.id.getOrElse {
+    throw new IllegalStateException("Running with a competition lacking an ID")
+  }
+  var csvContent: Option[String] = None
+
+  def csvUploadHandler(file: FileParamHolder) = {
+    println("saw file upload")
+    csvContent = Some(new String(file.file, "UTF-8"))
+  }
+
+  def doUpload = {
+    val teams = for {
+      csv <- csvContent.toSeq
+      csvRow <- csv.lines
+      Array(teamName, teamOrg) = csvRow.split(",")
+      team = Team(None, competitionId, teamName, teamOrg)
+      _ <- DB.runAwait(Teams.insertOrUpdate(team))
+    } yield {
+      team
+    }
+
+    S.redirectTo(TeamList.menu.toLoc.calcHref(competition), () => S.notice(s"Uploaded ${teams.length} teams"))
+  }
+
+  def render = {
+    "#team-csv-file" #> SHtml.fileUpload(csvUploadHandler _) &
+    ".upload" #> SHtml.onSubmitUnit(doUpload _)
+  }
+}
