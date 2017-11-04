aed123f9207563a8c8d4a3ff48a2f7469010d485
Matt Farmer <matt@frmr.me>
2017-11-04 14:07:24 -0400

    Implement bindings for SponsorForm

--- src/main/scala/frmr/scyig/webapp/snippet/admin/SponsorForm.scala
+++ src/main/scala/frmr/scyig/webapp/snippet/admin/SponsorForm.scala
@@ -5,10 +5,12 @@ import frmr.scyig.db._
 import frmr.scyig.webapp.auth.AuthenticationHelpers._
 import frmr.scyig.webapp.snippet.SnippetHelpers._
 import net.liftweb.http._
+import net.liftweb.http.js.JsCmds._
 import net.liftweb.sitemap._
 import net.liftweb.sitemap.Loc._
 import net.liftweb.util._
 import net.liftweb.util.Helpers._
+import slick.jdbc.MySQLProfile.api._
 
 object SponsorForm {
   import SnippetHelpers._
@@ -25,4 +27,22 @@ object SponsorForm {
   ) / "admin" / "sponsors" / "edit" / * >>
     validateSuperuser >>
     TemplateBox( () => Templates("admin" :: "sponsors" :: "form" :: Nil) )
+}
+
+class SponsorForm(sponsor: Sponsor) {
+  def this() = this(Sponsor(None, "", ""))
+
+  var currentSponsor = sponsor
+
+  private[this] def saveSponsor() = {
+    DB.runAwait(Sponsors.insertOrUpdate(currentSponsor))
+    RedirectTo(SponsorList.menu.loc.calcDefaultHref, () => S.notice("Sponsor was saved"))
+  }
+
+  def render = {
+    SHtml.makeFormsAjax andThen
+    "#sponsor-name" #> SHtml.text(sponsor.name, v => currentSponsor = currentSponsor.copy(name = v)) &
+    "#sponsor-location" #> SHtml.text(sponsor.location, v => currentSponsor = currentSponsor.copy(location = v)) &
+    ".save" #> SHtml.ajaxOnSubmit(saveSponsor _)
+  }
 }
